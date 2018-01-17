b1c46b228d39649f62a76e51a16a041e683e52e3
Mihwa Han <mhan3141@gmail.com>
2018-01-17 00:00:15 -0500

    add: add comments to clarify steps

--- Capstone_Project/final_code.py
+++ Capstone_Project/final_code.py
@@ -1,5 +1,4 @@
 ## Python Package
-
 import cv2
 import numpy as np
 from tqdm import tqdm
@@ -74,7 +73,9 @@ print(train_tensors.shape)
 print(valid_tensors.shape)
 print(test_tensors.shape)
 
+
 ## Model 1
+## Build Model 1, adding each layer 
 model1 = Sequential()
 model1.add(Conv2D(filters=32,kernel_size=2, activation='relu',
                  input_shape=train_tensors.shape[1:]))
@@ -92,6 +93,7 @@ model1.add(Dense(1024,activation='relu'))
 model1.add(Dense(12,activation='softmax'))
 
 ## Model 3
+## Build Model 3, adding each layer
 model3 = Sequential()
 model3.add(Conv2D(filters=64,kernel_size=2, activation='relu',
                  input_shape=train_tensors.shape[1:]))
@@ -112,21 +114,23 @@ model3.add(Dropout(0.2))
 model3.add(GlobalAveragePooling2D())
 model3.add(Dense(512,activation='relu'))
 model3.add(Dropout(0.2))
-
 model3.add(Dense(256,activation='relu'))
 model3.add(Dropout(0.2))
-
 model3.add(Dense(12,activation='softmax'))
 
 ## Final : Train Model 1 (rmsprop)
 model1.compile(optimizer=RMSprop(), loss='categorical_crossentropy',metrics=['accuracy'])
 
+## save the model weight using ModelCheckpoint
 checkpointer = ModelCheckpoint(filepath='weights.model1_rmsprop.hdf5', 
                            verbose=1, save_best_only=True)
+
+## train the model1 with train dataset and validation dataset with epoch=20 and batch_size=20
 model1.fit(train_tensors, y_train, 
           validation_data=(valid_tensors, y_valid),
           epochs=20, batch_size=20, callbacks=[checkpointer], verbose=1)
 
+## load the weight and make a prediction using test data set.
 model1.load_weights('weights.model1_rmsprop.hdf5')
 predictions = [np.argmax(model1.predict(np.expand_dims(feature, axis=0))) 
                for feature in test_tensors]
@@ -136,15 +140,13 @@ test_list = y_test.argmax(axis=1)
 print(f1_score(test_list, predictions, average='macro')) 
 print(accuracy_score(test_list,predictions))
 
-## Train Model 1 (Image augmentation)
+## train model1 with image augmentation using ImageDataGenerator with epoch =50 and batch_size=50
+epochs=50
+batch_size=32
 model1.compile(optimizer=RMSprop(), loss='categorical_crossentropy',metrics=['accuracy'])
-
 checkpointer = [EarlyStopping(monitor='val_loss', patience=5, verbose=0), 
             ModelCheckpoint(filepath='weights.model1_rmsprop_with_aug.hdf5', monitor='val_loss', save_best_only=True, verbose=0),
             ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, verbose=0, mode='auto', epsilon=0.0001, cooldown=0, min_lr=0)]
-
-epochs=50
-batch_size=32
 
 datagen = ImageDataGenerator(horizontal_flip=True,
                              vertical_flip=True)
@@ -157,7 +159,7 @@ model1.fit_generator(datagen.flow(train_tensors, y_train, batch_size=batch_size)
                     epochs=epochs, 
                     verbose=1)
 
-## Result Check
+## load the weight and make a prediction using test data set.
 model1.load_weights('weights.model1_rmsprop_with_aug.hdf5')
 predictions = [np.argmax(model1.predict(np.expand_dims(feature, axis=0))) 
                for feature in test_tensors]
@@ -167,10 +169,8 @@ test_list = y_test.argmax(axis=1)
 print(f1_score(test_list, predictions, average='weighted')) 
 print(accuracy_score(test_list,predictions))
 
-print(precision_score(test_list, predictions, average='micro'))  
-print(recall_score(test_list, predictions, average='micro'))  
 
-## Confusion Matrix
+## Check a confusion matrix with the result using test dataset.
 confusion = confusion_matrix(test_list,predictions)
 abbreviation = ['BG', 'Ch', 'Cl', 'CC', 'CW', 'FH', 'LSB', 'M', 'SM', 'SP', 'SFC', 'SB']
 pd.DataFrame({'class': categories, 'abbreviation': abbreviation})
@@ -186,8 +186,7 @@ pd.DataFrame({'class': categories, 'abbreviation': abbreviation})
 # plt.xlabel('Predicted',size=16)
 # plt.show();
 
-## apply the model to the test file and save the result
-
+## Apply the model to the test file and save the result for kaggle posting
 model1.load_weights('weights.model1_rmsprop_with_aug.hdf5')
 data=load_files('./test')
 final_X_test = np.array(data['filenames'])
@@ -202,6 +201,7 @@ df_sort = df.sort_values(by=['file'])
 df_sort.to_csv('final.csv', index=False)
 
 ## Appendix : Model with Xception
+## I used the image size with 128 x 128 using Xception (47 x 47 is not allowed to use Xception model)
 labels = listdir("./train")
 train_files, train_targets = load_dataset('./train')
 
@@ -218,21 +218,27 @@ for train_index, test_index in sss.split(valid_tensors, y_valid):
     valid_tensors, test_tensors = valid_tensors[train_index], valid_tensors[test_index]
     y_valid, y_test = y_valid[train_index], y_valid[test_index]
 
-pre_train = Xception(input_shape=(128,128, 3), include_top=False, weights='imagenet', pooling='avg') 
+## load Xception model 
+pre_train = Xception(input_shape=(128,128, 3), include_top=False, weights='imagenet', pooling='avg')
+
+## complete the model with Xception with a fully connected layer (and dropout)
 x = pre_train.output
 x = Dense(128, activation='relu')(x)
 x = Dropout(0.2)(x)
 predictions = Dense(12, activation='softmax')(x)
 model_Xception = Model(inputs=pre_train.input, outputs=predictions)
 
+## compile the model with Xception using adam algorithm as an optimizer.
 model_Xception.compile(optimizer=Adam(lr=0.001), loss='categorical_crossentropy',metrics=['accuracy']) 
 
 checkpointer = ModelCheckpoint(filepath='weights.Xception.hdf5', 
                                verbose=1, save_best_only=True)
+## train the model
 model_Xception.fit(train_tensors, y_train, 
           validation_data=(valid_tensors, y_valid),
           epochs=5, batch_size=32, callbacks=[checkpointer], verbose=1)
 
+## load the model weights and predict with test dataset.
 model.load_weights('weights.Xception.hdf5')
 predictions = [np.argmax(model_Xception.predict(np.expand_dims(feature, axis=0))) 
                for feature in test_tensors]
@@ -242,7 +248,7 @@ test_list = y_test.argmax(axis=1)
 print(f1_score(test_list, predictions, average='weighted')) 
 print(accuracy_score(test_list,predictions))
 
-
+## train the model with Image Augmentation using ImageDataGenerator
 model_Xception.compile(loss='categorical_crossentropy', optimizer=optimizers.Adam(), metrics=['accuracy'])
 
 datagen = ImageDataGenerator( horizontal_flip=True, 
@@ -261,12 +267,12 @@ model_Xception.fit_generator(datagen.flow(train_tensors, y_train, batch_size=bat
                     epochs=epochs, 
                     verbose=1)
 
-
+## load the model weights and predict with test dataset
 model_Xception.load_weights('weights.Xception_with_aug.hdf5')
 predictions = [np.argmax(model_Xception.predict(np.expand_dims(feature, axis=0))) 
                for feature in test_tensors]
 y_pred = [labels[i] for i in predictions]
 test_list = y_test.argmax(axis=1)
-from sklearn.metrics import accuracy_score, log_loss, f1_score
+
 print(f1_score(test_list, predictions, average='weighted')) 
 print(accuracy_score(test_list,predictions))
